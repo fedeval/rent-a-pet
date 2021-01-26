0f79fc16bd48841c9ad12c130e7e3e3f071fd329
Leonardo Dacoreggio <dacoreggio@gmail.com>
2021-01-26 21:15:28 +0100

    validations done, except for confirmed field

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pet
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :price, presence: true
+  validates :user_id, presence: true
+  validates :pet_id, presence: true
 end

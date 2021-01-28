19e95adb4b2ca4e326ce81c5ac55b794072c3f44
Federico Valenza <federico.valenza5@gmail.com>
2021-01-28 21:14:04 +0100

    added new and create user actions

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,6 @@
 class PetsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @pets = policy_scope(Pet)
   end
@@ -9,9 +11,19 @@ class PetsController < ApplicationController
   end
 
   def new
+    @pet = Pet.new
+    authorize @pet
   end
 
   def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+    authorize @pet
   end
 
   def edit
@@ -25,5 +37,13 @@ class PetsController < ApplicationController
     authorize @pet
     @pet.destroy
     redirect_to pets_path
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :age,
+                                :description, :price_per_day, :location,
+                                :available)
   end
 end

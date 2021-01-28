2de3514f978ed86eaa96c8052f8e6261bca06351
Leonardo Dacoreggio <dacoreggio@gmail.com>
2021-01-28 19:14:08 +0100

    bookings model validates price greated than zero

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,11 @@ class Booking < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
   validates :user_id, presence: true
   validates :pet_id, presence: true
+
+  # def end_date_possible?
+  #   :end_date > :start_date
+  # end
 end

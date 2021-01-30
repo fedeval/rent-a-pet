170029a04d8095bde54eaa3ec10f0e2229857e22
Federico Valenza <federico.valenza5@gmail.com>
2021-01-30 11:46:37 +0100

    geocoder config

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -13,4 +13,7 @@ class Pet < ApplicationRecord
   validates :price_per_day, presence: true, numericality: { greater_than: 0 }
   validates :location, presence: true
   validates :available, presence: true
+ 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_200613) do
+ActiveRecord::Schema.define(version: 2021_01_30_104337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2021_01_26_200613) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
